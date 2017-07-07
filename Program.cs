f08521ed25cb241ae71bafd4f598958f2420c007
arock83 <arock83@gmail.com>
2017-07-07 13:54:48 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+             List<string> planetList = new List<string>(){"Mercury", "Mars"};
+             planetList.Add("Jupiter");
+             planetList.Add("Saturn");
+
+             List<string> lastPlanets = new List<string>{
+                 "Neptune", "Uranus"
+             };
+
+             planetList.AddRange(lastPlanets);
+             planetList.Insert(1, "Venus");
+             planetList.Insert(2, "Earth");
+             planetList.Add("Pluto");
+
+             List<string> rockyPlanets = new List<string>();
+
+             rockyPlanets = planetList.GetRange(0, 4);
+
+             foreach(string planet in rockyPlanets){
+
+                 Console.WriteLine(planet);
+             }
+
+             planetList.Remove("Pluto");
+
+             Console.WriteLine("**************");
+
+             foreach(string planet in planetList){
+                 Console.WriteLine(planet);
+             }
+             
+        }
+    }
+}
